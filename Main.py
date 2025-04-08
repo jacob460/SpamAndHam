9e415e9405976025d632acc28288e283e21efb1b
jacob460 <65002885+jacob460@users.noreply.github.com>
2025-04-08 00:35:23 -0400

    training data split and recorded

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import string
 import csv
 import random
 import os
@@ -41,22 +42,39 @@ class SpamFilter:
 
 
     def clean_data(self):
+        #print(string.punctuation)
         text = []
         for x in self.raw_data:
-            temp_string = x[1]
-            temp_string = temp_string.strip()
-            temp_string = temp_string.strip()
+            if not (x in stopwords.words('english')):
+                temp_string = x[1].lower()
+                for y in string.punctuation:
+                    temp_string = temp_string.replace(y, "")
+                temp_string = temp_string.strip()
+                temp_string = temp_string.strip()
+                temp_string = temp_string.split()
+                for y in temp_string:
+                    self.vocab.add(y)
             text.append([x[0], temp_string])
-        print(text)
-        pass
+        self.raw_data = text
+    pass
 
     def train(self):
-        pass
+        for x in self.raw_data:
+            if x[0] == 'ham':
+                for y in x[1]:
+                    self.ham_words[y] += 1
+            else:
+                for y in x[1]:
+                    self.spam_words[y] += 1
+
 
     def predict(self, text):
         pass
 
-data = load_data('spam.csv')
+data = load_data('spam.csv')[1:]
 train, test = split_data(data)
 model = SpamFilter(data, smoothing=1)
 model.clean_data()
+print('rawData: ', model.raw_data[:5])
+model.train()
+print(f'HAM: \n{model.ham_words}\n\nSPAM: \n{model.spam_words}')
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import string
 import csv
 import random
 import os
@@ -8,8 +9,12 @@ import collections
 from nltk.corpus import stopwords
 
 def load_data(path):
-    with open(path) as csvfile:
-        reader = list(csv.reader(csvfile))
+    try:
+        with open(path) as csvfile:
+            reader = list(csv.reader(csvfile))
+    except UnicodeDecodeError:
+        with open(path, encoding='latin-1') as csvfile:
+            reader = list(csv.reader(csvfile))
     return reader
 
 def split_data(data):
@@ -37,13 +42,39 @@ class SpamFilter:
 
 
     def clean_data(self):
-        pass
+        #print(string.punctuation)
+        text = []
+        for x in self.raw_data:
+            if not (x in stopwords.words('english')):
+                temp_string = x[1].lower()
+                for y in string.punctuation:
+                    temp_string = temp_string.replace(y, "")
+                temp_string = temp_string.strip()
+                temp_string = temp_string.strip()
+                temp_string = temp_string.split()
+                for y in temp_string:
+                    self.vocab.add(y)
+            text.append([x[0], temp_string])
+        self.raw_data = text
+    pass
 
     def train(self):
-        pass
+        for x in self.raw_data:
+            if x[0] == 'ham':
+                for y in x[1]:
+                    self.ham_words[y] += 1
+            else:
+                for y in x[1]:
+                    self.spam_words[y] += 1
+
 
     def predict(self, text):
         pass
 
-data = load_data('spam.csv')
-split_data(data)
+data = load_data('spam.csv')[1:]
+train, test = split_data(data)
+model = SpamFilter(data, smoothing=1)
+model.clean_data()
+print('rawData: ', model.raw_data[:5])
+model.train()
+print(f'HAM: \n{model.ham_words}\n\nSPAM: \n{model.spam_words}')
