148057babfc99dde4dc92e1c4cfa26ed5012bbd9
jacob460 <65002885+jacob460@users.noreply.github.com>
2025-04-08 01:56:51 -0400

    Fixed smoothing
    fixed smoothing

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ import math
 import collections
 from nltk.corpus import stopwords
 
+## TODO: Add the ability to save and retrieve your model as a serialized object.
+## TODO: Complete text_accuracy method
+## TODO: Complete predict method
+
+
 def load_data(path):
     try:
         with open(path) as csvfile:
@@ -28,6 +33,10 @@ def split_data(data):
     return training_data, testing_data
 
 def test_accuracy(data):
+    # Complete the test_accuracy method. Send it the testing data and report on its accuracy.
+    # Use a confusion matrix and include at least accuracy, precision, and recall.
+    # You can read about a confusion matrix here:
+    # https://www.coursera.org/articles/what-is-a-confusion-matrix. Links to an external site.
     pass
 
 
@@ -44,6 +53,7 @@ class SpamFilter:
     def clean_data(self):
         #print(string.punctuation)
         text = []
+        temp_string = ''
         for x in self.raw_data:
             if not (x in stopwords.words('english')):
                 temp_string = x[1].lower()
@@ -59,6 +69,9 @@ class SpamFilter:
     pass
 
     def train(self):
+        for x in self.vocab:
+            self.ham_words[x] += self.smoothing
+            self.spam_words[x] += self.smoothing
         for x in self.raw_data:
             if x[0] == 'ham':
                 for y in x[1]:
@@ -66,9 +79,18 @@ class SpamFilter:
             else:
                 for y in x[1]:
                     self.spam_words[y] += 1
+        pass
 
 
-    def predict(self, text):
+
+    def predict(self, text):   #Complete the predict method so it can receive a text string and will return a ham or spam classification. Use a logarithmic scale.
+        # https://www.saedsayad.com/naive_bayesian.htm
+        #P(c|X) = P(x1|c)*P(x2|c)*...*P(xn|c)*P(c)
+        #P(c|x) = P(x|c)P(c) / P(x)
+        #P(c | x) posterior probability of class(target) given predictor(attribute)
+        #P(x | c) prior probability of class
+        # P(c) is the likelihood which is the probability of predictor given class
+        # P(x) is the prior probability of predictor
         pass
 
 data = load_data('spam.csv')[1:]
@@ -78,3 +100,4 @@ model.clean_data()
 print('rawData: ', model.raw_data[:5])
 model.train()
 print(f'HAM: \n{model.ham_words}\n\nSPAM: \n{model.spam_words}')
+
