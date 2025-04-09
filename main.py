35c4edbc4f1a6278926564d4bbe10c2f10a749ea
jacob460 <65002885+jacob460@users.noreply.github.com>
2025-04-09 02:29:15 -0400

    Finished TODO except serializable export
    need to check logic and add serializable export

--- main.py
+++ main.py
@@ -6,12 +6,9 @@ import os
 import email
 import math
 import collections
+from zipfile import error
+
 from nltk.corpus import stopwords
-
-## TODO: Add the ability to save and retrieve your model as a serialized object.
-## TODO: Complete text_accuracy method
-## TODO: Complete predict method
-
 
 def load_data(path):
     try:
@@ -23,20 +20,54 @@ def load_data(path):
     return reader
 
 def split_data(data):
-    testing_data = data.copy()
-    testing_percentage = 0.4
+    training_data = data.copy()
+    testing_percentage = 0.6
     training_percentage = 1 - testing_percentage
-    training_size = len(data)*training_percentage
-    training_data = []
-    while len(training_data) < training_size:
-        training_data.append(testing_data.pop(random.randint(0, int(len(testing_data) - 1))))
+    training_size = int(len(data)*training_percentage)
+    testing_data = []
+    while len(training_data) > training_size:
+        x = random.randint(0, int(len(training_data) - 1 ))
+        temp = training_data.pop(x)
+        testing_data.append(temp)
     return training_data, testing_data
 
 def test_accuracy(data):
-    # Complete the test_accuracy method. Send it the testing data and report on its accuracy.
-    # Use a confusion matrix and include at least accuracy, precision, and recall.
-    # You can read about a confusion matrix here:
-    # https://www.coursera.org/articles/what-is-a-confusion-matrix. Links to an external site.
+    TP, TN, FP, FN = 0,0,0,0
+    train, test = split_data(data)
+    print('train len: ', len(train))
+    print('test len: ',len(test))
+    model = SpamFilter(train, smoothing=1)
+    model.clean_data()
+    # print('rawData: ', model.raw_data[:5])
+    model.train()
+    #print('Raw: \n\t', test)
+    result = []
+    for x in test:
+        result.append(model.predict(x[1]))
+    #print('\n\nResult: \n\t', result)
+    for index, value in enumerate(result):
+        if value[0] == 'ham' and test[index][0] == 'ham':
+            TP += 1
+        elif value[0] == 'ham' and test[index][0] == 'spam':
+            FP += 1
+        elif value[0] == 'spam' and test[index][0] == 'spam':
+            TN += 1
+        elif value[0] == 'spam' and test[index][0] == 'ham':
+            FN += 1
+    accuracy = (TP + TN) / (TP + TN + FP + FN)
+    precision = TP / (TP + FP)
+    recall = TP / (TP + FN)
+    print(test[:5])
+    print(result[:5])
+    print('{:<10} {:>10} {:>10}'.format('','Pred Pos','Pred Neg'))
+    print('{:<10} {:>10} {:>10}'.format('True Pos', TP, FN))
+    print('{:<10} {:>10} {:>10}'.format('True Neg',FP,TN))
+
+    print('Accuracy: ', accuracy)
+    print('Precision: ', precision)
+    print('Recall: ', recall)
+
+
     pass
 
 
@@ -55,16 +86,18 @@ class SpamFilter:
         text = []
         temp_string = ''
         for x in self.raw_data:
-            if not (x in stopwords.words('english')):
-                temp_string = x[1].lower()
-                for y in string.punctuation:
-                    temp_string = temp_string.replace(y, "")
-                temp_string = temp_string.strip()
-                temp_string = temp_string.strip()
-                temp_string = temp_string.split()
-                for y in temp_string:
-                    self.vocab.add(y)
-            text.append([x[0], temp_string])
+            temp_tokens = []
+            temp_string = x[1].lower()
+            for y in string.punctuation:
+                temp_string = temp_string.replace(y, "")
+            temp_string = temp_string.strip()
+            temp_string = temp_string.split()
+            for y in temp_string:
+                if not y in stopwords.words('english'):
+                    temp_tokens.append(y)
+            for y in temp_tokens:
+                self.vocab.add(y)
+            text.append([x[0], temp_tokens])
         self.raw_data = text
     pass
 
@@ -85,19 +118,63 @@ class SpamFilter:
 
     def predict(self, text):   #Complete the predict method so it can receive a text string and will return a ham or spam classification. Use a logarithmic scale.
         # https://www.saedsayad.com/naive_bayesian.htm
+        #P(A|B) = P(AnB)/P(B)   A given B
         #P(c|X) = P(x1|c)*P(x2|c)*...*P(xn|c)*P(c)
         #P(c|x) = P(x|c)P(c) / P(x)
         #P(c | x) posterior probability of class(target) given predictor(attribute)
         #P(x | c) prior probability of class
         # P(c) is the likelihood which is the probability of predictor given class
         # P(x) is the prior probability of predictor
-        pass
+        temp_string = ''
+        temp_tokens = []
+        temp_string = text.lower()
+        total_spam = 0
+        total_ham = 0
+        total_words_spam = sum(self.spam_words.values())
+        total_words_ham = sum(self.ham_words.values())
+        p_spam = 0
+        p_ham = 0
+        for y in string.punctuation:
+            temp_string = temp_string.replace(y, "")
+        temp_string = temp_string.strip()
+        temp_string = temp_string.split()
+        for y in temp_string:
+            if not (y in stopwords.words('english')) and (y in self.vocab):
+                temp_tokens.append(y)
+        for x in self.raw_data:
+            if x[0] == 'ham':
+                total_ham += 1
+            elif x[0] == 'spam':
+                total_spam += 1
+        p_spam = math.log(total_spam/(total_ham+total_spam))
+        p_ham = math.log(total_ham/(total_spam+total_ham))
+        #P(A|B) = P(AnB)/P(B)   A given B
+        p_word_spam = []
+        p_word_ham = []
+        for x in temp_string:
+            if x in self.vocab:
+                #print(total_words_spam, self.spam_words[x], self.ham_words[x])
+                p_word_spam.append([x, math.log(self.spam_words[x] / total_words_spam)])
+                p_word_ham.append([x, math.log(self.ham_words[x] / total_words_ham)])
+
+        #print('HAM', p_word_ham)
+        #print('T HAM', total_words_ham)
+        #print('SPAM', p_word_spam)
+        #print('T SPAM', total_words_spam)
+        sum_p_word_spam = p_spam
+        sum_p_word_ham = p_ham
+        for x in p_word_spam:
+            sum_p_word_spam += x[1]
+        for x in p_word_ham:
+            sum_p_word_ham += x[1]
+        #print(f"spam: {sum_p_word_spam} vs ham: {sum_p_word_ham}")
+        if sum_p_word_ham > sum_p_word_spam:
+            return ['ham', text]
+        else:
+            return ['spam', text]
 
 data = load_data('spam.csv')[1:]
-train, test = split_data(data)
-model = SpamFilter(data, smoothing=1)
-model.clean_data()
-print('rawData: ', model.raw_data[:5])
-model.train()
-print(f'HAM: \n{model.ham_words}\n\nSPAM: \n{model.spam_words}')
-
+test_accuracy(data)
+test_accuracy(data)
+test_accuracy(data)
+test_accuracy(data)
